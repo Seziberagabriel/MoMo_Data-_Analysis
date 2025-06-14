2079a72da097cf2dd1863af1df8f90afcb73c3e3
pmuwanguzi <p.muwanguzi@alustudent.com>
2025-06-14 17:35:19 +0200

    added flask_resful plugin

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,10 +1,11 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy 
-
+from flask_restful import Resource, Api, reparse, reqparse, fields , marshal_with, abort
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+api = Api(app)
 
 
 
