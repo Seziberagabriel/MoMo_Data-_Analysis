41acfeb76bfbfd2b21296ad5dac21d795b1e2b8b
pmuwanguzi <p.muwanguzi@alustudent.com>
2025-06-14 17:29:06 +0200

    created db creater

--- backend/app/create_db.py
+++ backend/app/create_db.py
@@ -1,2 +1,5 @@
 from api import app, db
 from models import UserModel
+
+with app.app_context():
+    db.create_all()
--- backend/app/models.py
+++ backend/app/models.py
@@ -5,8 +5,8 @@ from api import db
 
 class UserModel(db.Model):
     id = db.Column(db.Integer,primary_key=True)
-    user_name = db.Column(db.String(80),unique=True,nullalbe=False)
-    email = db.Column(db.String(80),unique=True,nullalbe=False)
+    user_name = db.Column(db.String(80),unique=True,nullable=False)
+    email = db.Column(db.String(80),unique=True,nullable=False)
 
     def __repr__(self):
         return f"User(name = {self.user_namename}, email = {self.email})"
